ebb2779812a61890b94d224232e605884bce72bc
NjabuloNeslo <nthwala@neslotech.co.za>
2022-04-02 03:53:52 +0200

    QUOT: Welcome loading

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,60 +12,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@1,900&family=Righteous&display=swap"
           rel="stylesheet">
     <script src="https://kit.fontawesome.com/9ecd261fd1.js" crossorigin="anonymous"></script>
-    <!-- Styles -->
-    <style>
-        html {
-
-        }
-
-        body {
-            padding: 50px;
-            background-image: url('http://localhost:8000/back.jpeg');
-        }
-
-        header {
-            border-bottom: solid whitesmoke 5px;
-            padding-bottom: 15px;
-            display: flex;
-        }
-
-        .header-logo-div, .header-social-media-div {
-            display: inline-block;
-        }
-
-        .header-img {
-            border-radius: 200px;
-        }
-
-        .header-div-social {
-            margin-left: 10px;
-            width: 100%
-        }
-
-        .header-logo-div {
-            font-family: 'Righteous', cursive;
-            font-size: 2rem;
-            color: #f9a602;
-        }
-
-        .header-social-media-div {
-            float: right;
-        }
-
-        .header-social-li {
-            color: white;
-            display: inline-block;
-        }
-
-        li:not(:first-of-type) {
-            margin-left: 30px;
-        }
-
-        .body-welcome-strong {
-            color: #f9a602;
-        }
-
-    </style>
+    <link rel="stylesheet" href="http://localhost:8000/css/app.css">
 </head>
 <body>
 <header>
@@ -91,17 +38,26 @@
         </div>
     </div>
 </header>
+<style>
 
-<div style="color:white;text-align: center; margin-top: 100px; padding: 20px;">
-    <h1 style="margin: 0px; font-family: 'Montserrat', sans-serif; font-size: 2rem">
-        <strong class="body-welcome-strong">&lt;html&gt;</strong>
-        Building a front end, no problemo.
-        <strong class="body-welcome-strong">&lt;/html&gt;</strong>
-    </h1>
+</style>
+<div class="body-welcome-div">
+    <div>
+        <h1 class="margin-0 welcome-strong-h1">
+            <strong class="body-welcome-strong">&lt;html&gt;</strong>
+            Building a front end, no problemo.
+            <strong class="body-welcome-strong">&lt;/html&gt;</strong>
+        </h1>
 
-    <h1 style="margin: 0px; font-family: 'Montserrat', sans-serif; font-size: 5rem">
-        Try our FREE apis
-    </h1>
+        <h1 class="margin-0 welcome-strong-h1-2">
+            Try our FREE apis
+        </h1>
+    </div>
+    <div class="margin-top-100 fa-3x">
+        <i class="fa-solid fa-spinner fa-xl fa-spin-pulse"></i>
+        <i class="fa-solid fa-spinner fa-xl fa-spin-pulse fa-spin-reverse"></i>
+    </div>
+
 </div>
 </body>
 </html>
